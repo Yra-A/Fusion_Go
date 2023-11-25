842bb11af89c4db21e2bc87b85ff2374c529b162
Yra-A <1325083019@qq.com>
2023-11-25 22:49:42 +0800

    fix: 增加了环境变量 accesskey

--- pkg/configs/oss/configs.go
+++ pkg/configs/oss/configs.go
@@ -1,10 +1,16 @@
 package oss
 
+import "os"
+
+var (
+	AccessKeyID     = os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_ID")
+	AccessKeySecret = os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET")
+)
+
 const (
-	EndPoint        = "oss-cn-shanghai.aliyuncs.com"
-	AccessKeyID     = ""
-	OssBucketName   = "yra-fusion"
-	BaseURL         = "public/fusion/"
-	AccessKeySecret = ""
-	PublicURL       = "https://yra-fusion.oss-cn-shanghai.aliyuncs.com/" + BaseURL
+	EndPoint = "oss-cn-shanghai.aliyuncs.com"
+
+	OssBucketName = "yra-fusion"
+	BaseURL       = "public/fusion/"
+	PublicURL     = "https://yra-fusion.oss-cn-shanghai.aliyuncs.com/" + BaseURL
 )
